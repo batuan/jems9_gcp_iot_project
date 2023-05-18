d7af688d2fbaa17094735beba17d5fc96170f7a1
quanticali <alib612@protonmail.com>
2023-05-18 15:38:24 +0200

    python3 script/random_data.py -n 2 -s ttrr_

--- script/random_data.py
+++ script/random_data.py
@@ -3,7 +3,8 @@
 
 # In[14]:
 
-
+import sys
+import getopt
 import pandas as pd
 import random
 import datetime
@@ -11,117 +12,127 @@ import threading
 import time
 import json
 import paho.mqtt.client as mqtt
+import sched
 
 # Connect to the MQTT broker
 client = mqtt.Client()
 
-if client.connect("0.0.0.0", 1883, 60)!=0:
+if client.connect("0.0.0.0", 1883, 60) != 0:
     print("could not connect to mqqt broker!")
     import sys
     sys.exit(-1)
 
 # Create the message
 message = {
-  "dateHour": "2023-05-08T14:40:00",
-  "gpsSpeed": 60,
-  "gpsSatCount": 8,
-  "Gear": "3",
-  "Brake_pedal": 0.5,
-  "Accel_pedal": 0.8
+    "dateHour": "2023-05-08T14:40:00",
+    "gpsSpeed": 60,
+    "gpsSatCount": 8,
+    "Gear": "3",
+    "Brake_pedal": 0.5,
+    "Accel_pedal": 0.8
 }
 json_message = json.dumps(message)
 
-# Publish the message to the "vehicle/sensor_data" topic
+# Publish the message to the "vehicuels" topic
 
-header = "dateHour;gpsSpeed;gpsSatCount;Gear;Brake_pedal;Accel_pedal;Machine_Speed_Mesured;AST_Direction;Ast_HPMB1_Pressure_bar;Ast_HPMA_Pressure_bar;Pressure_HighPressureReturn;Pressure_HighPressure;Oil_Temperature;Ast_FrontAxleSpeed_Rpm;Pump_Speed"
+header = "dateHour;gpsSpeed;gpsSatCount;Gear;Brake_pedal;Accel_pedal;Machine_Speed_Mesured;AST_Direction;Ast_HPMB1_Pressure_bar;Ast_HPMA_Pressure_bar;Pressure_HighPressureReturn;Pressure_HighPressure;Oil_Temperature;Ast_FrontAxleSpeed_Rpm;Pump_Speed; sensorID"
 
 row_count = 0
-file_count = 0
-max_rows_per_file = 10
+max_rows_per_file = 10000
 data_buffer = []
 start_time = time.time()
 
-def rand():
-    dateHour = datetime.datetime.now()
-    gpsSpeed = str(random.uniform(0, 87.06))
-    gpsSatCount	= random.randint(0, 255)
+def rand_data():
     Gear = random.randint(124, 137)
-    Brake_pedal	= Gear
-    Accel_pedal	= random.randint(0, 100)
-    Machine_Speed_Mesured = 20
-    AST_Direction = 20
-    Ast_HPMB1_Pressure_bar = random.randint(0, 13)
-    Ast_HPMA_Pressure_bar = random.randint(0, 13)
-    Pressure_HighPressureReturn	= random.randint(0, 13)
-    Pressure_HighPressure = random.randint(32767, 33195)
-    Oil_Temperature	= random.randint(11, 255)
-    Ast_FrontAxleSpeed_Rpm = random.randint(32767, 33195)
-    Pump_Speed = random.randint(577, 1799)
-
-    message = {
-    'dateHour' : str(datetime.datetime.now()),
-    'gpsSpeed' : str(random.uniform(0, 87.06)),
-    'gpsSatCount'	: random.randint(0, 255),
-    'Gear' : random.randint(124, 137),
-    'Brake_pedal' : Gear,
-    'Accel_pedal' : random.randint(0, 100),
-    'Machine_Speed_Mesured' : 20,
-    'AST_Direction' : 20,
-    'Ast_HPMB1_Pressure_bar' : random.randint(0, 13),
-    'Ast_HPMA_Pressure_bar' : random.randint(0, 13),
-    'Pressure_HighPressureReturn' : random.randint(0, 13),
-    'Pressure_HighPressure' : random.randint(32767, 33195),
-    'Oil_Temperature' : random.randint(11, 255),
-    'Ast_FrontAxleSpeed_Rpm' : random.randint(32767, 33195),
-    'Pump_Speed' : random.randint(577, 1799),
+    rndData = {
+        'dateHour': str(datetime.datetime.now()),
+        'gpsSpeed': random.uniform(0, 87.06),
+        'gpsSatCount': random.randint(0, 255),
+        'Gear': Gear,
+        'Brake_pedal': Gear,
+        'Accel_pedal': random.randint(0, 100),
+        'Machine_Speed_Mesured': 20,
+        'AST_Direction': 20,
+        'Ast_HPMB1_Pressure_bar': random.randint(0, 13),
+        'Ast_HPMA_Pressure_bar': random.randint(0, 13),
+        'Pressure_HighPressureReturn': random.randint(0, 13),
+        'Pressure_HighPressure': random.randint(32767, 33195),
+        'Oil_Temperature': random.randint(11, 255),
+        'Ast_FrontAxleSpeed_Rpm': random.randint(32767, 33195),
+        'Pump_Speed': random.randint(577, 1799)
     }
+    return rndData
 
-    return (str(dateHour) + ';' + str(gpsSpeed) + ';' + str(gpsSatCount) + ';' + str(Gear) + ';' + str(Brake_pedal) + 
-            ';' + str(Accel_pedal) + ';' + str(Machine_Speed_Mesured) + ';' + str(AST_Direction) + ';' +
-            str(Ast_HPMB1_Pressure_bar) + ';' + str(Ast_HPMA_Pressure_bar) + ';' + 
-            str(Pressure_HighPressureReturn) + ';' + str(Pressure_HighPressure) + ';' + 
-            str(Oil_Temperature) + ';' + str(Ast_FrontAxleSpeed_Rpm) + ';' + 
-            str(Pump_Speed)), message
 
 def save_data():
-    global file_count, data_buffer
+    global data_buffer
     if data_buffer:
-        file_count += 1
         current = datetime.datetime.now()
-        file_name = f"X467898_{str(current).replace(':', '_')}.csv"
+        file_name = "batchIoV_"+str(current).replace(':', '_')+".csv"
         rows = [row.split(';') for row in data_buffer]
         df = pd.DataFrame(rows, columns=header.split(';'))
-        # df.to_csv(file_name, index=False)
+        #df.to_csv(file_name, index=False)
         print(f"Saved {len(data_buffer)} rows to {file_name}")
         data_buffer = []
-    threading.Timer(60, save_data).start()
 
 
+def stream_data(sensorID, message):
+    message['sensorID'] = sensorID
+    topic = "vehicules"
+    result = client.publish(topic, json.dumps(message))
+    # result: [0, 1]
+    status = result[0]
+    if status != 0:
+        print(f"Failed to send message to topic {topic}")
 
-def printit():
-    global row_count, data_buffer, start_time
-    current = datetime.datetime.now()
-    row_count += 1
-    data, message = rand()
-    # print(message)
-    client.publish("vehicle/sensor_data", json.dumps(message))
-    data_buffer.append(data)
-    print(data)
+def one_sensor_data(sensorID):
+    global row_count, data_buffer
+    rndData = rand_data()
+    stream_data(sensorID, rndData)
+    csv_data = ';'.join(map(str,rndData.values()))
+    data_buffer.append(csv_data)
+    row_count+=1
+    
 
+def main(argv):
+    nbSensors = 1
+    sensorSuffix = ''
+    opts, args = getopt.getopt(argv, "hn:s:", ["nb=", "suffix="])
+    for opt, arg in opts:
+        if opt == '-h':
+            print('random_data.py -n numberOfSensors -s suffixName')
+            sys.exit()
+        elif opt in ("-n", "--nb"):
+            nbSensors = int(arg)
+        elif opt in ("-s", "--suffix"):
+            sensorSuffix = arg
+    print('nbSensors is ', nbSensors)
+    print('sensorSuffix is ', sensorSuffix)
+    my_scheduler = sched.scheduler(time.time, time.sleep)
+    my_scheduler.enter(0.1, 1, do_loop, (my_scheduler,nbSensors, sensorSuffix))
+    my_scheduler.run()
+
+def do_loop(scheduler, nbSensors, sensorSuffix): 
+    # schedule the next call first
+    scheduler.enter(0.1, 1, do_loop, (scheduler,nbSensors, sensorSuffix))
+    # then do your stuff
+    #print('row_count=', row_count)
+    threads = []
+    for i in range(nbSensors):
+        start = time.perf_counter()
+        sensorID = sensorSuffix+str(i)
+        t = threading.Thread(target=one_sensor_data(sensorID), args=(sensorID,))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
     if row_count % max_rows_per_file == 0:
         save_data()
-    if time.time() - start_time > 60:
-        print("Stopping threads...")
-        threading.Timer(1, print, args=("Stopping threads...",)).start()
-        for t in threading.enumerate():
-            if t != threading.current_thread():
-                t.cancel()
-    else:
-        threading.Timer(2, printit).start() # remember to set 0.1 in real time, 2 for debugging
+    finish = time.perf_counter()
+    #print(f'Finished in {round(finish-start, 2)} second(s)')
 
-save_data()
-printit()
-
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 # In[ ]:
 
